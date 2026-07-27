599027d88e6d998774532fa077d1aec60d0b90d5
Fareesuz Zama <121152882+Farees04@users.noreply.github.com>
2026-07-27 23:26:03 +0530

    Add requests import and update smoothie order logic

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,8 @@
 # Import python packages
 import streamlit as st
 from snowflake.snowpark.functions import col
+import requests  
+
 
 # Write directly to the app
 st.title(f":cup_with_straw: Example Streamlit App :cup_with_straw:")
@@ -25,6 +27,9 @@ if ingredient_list:
      ingredients_string = ''
      for fruit_chosen in ingredient_list:
          ingredients_string += fruit_chosen + ' '
+         smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
+         sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width = True)
+       
 
      # st.write(ingredient_list)    
 
@@ -33,10 +38,7 @@ if ingredient_list:
                     values ('""" + ingredients_string + """','"""+ name_on_order + """')"""
      st.write(ingredient_list) 
      time_to_insert = st.button('Submit Order')
-     import requests  
-     smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
      # st.text(smoothiefroot_response.json())
-     sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width = True)
 
      if time_to_insert:
          session.sql(my_insert_stmt).collect()
